07b5eff2dd46b5d2fb71609ffc0c5fd01c5b58a0
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2025-01-28 16:23:24 +0300

    music player (IoC)

--- src/main/java/ru/sharova/springapp/TestSpring.java
+++ src/main/java/ru/sharova/springapp/TestSpring.java
@@ -6,8 +6,8 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-        System.out.println(testBean.getName());
-        context.close();
+        Music music = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
     }
 }
