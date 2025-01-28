cbf6671e67f78fcf261ce6de30098e880e084b8c
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2025-01-28 18:27:43 +0300

    add test

--- src/main/java/ru/sharova/springapp/TestSpring.java
+++ src/main/java/ru/sharova/springapp/TestSpring.java
@@ -7,7 +7,10 @@ public class TestSpring {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        musicPlayer.playMusicList();
+        System.out.println("------------");
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
